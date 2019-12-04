657e7871092d643243c9f03aa06d2f3900c00857
Siddharth Mishra <smishra1605@gmail.com>
2019-12-04 11:40:11 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -19,14 +19,14 @@
         <br>
         <div id="form">
             <br>
-            <form action="" method="post" name="">
+            <form action="insert1.php" method="post">
             <label>Emoloyee Code</label><br>
                 <input type="text" name="e_code" style="width:90%; height: 30px;"><br><br>
                 <label>Password</label><br>
                 <input type="text" name="pwd" style="width:90%; height: 30px;"><br><br>
                 
                 <div id="button"><input type="Submit" value="LogIn" style="width: 90%; height: 50px; border-radius: 25px; border: none; font-family:cursive; font-size: 30px; background-color: azure;"></div><br><br>
-                <center>New User?<a href="signin.html" target="_self">SignIn Here</a></center>
+                <center>New User?<a href="signin.php" target="_self">SignIn Here</a></center>
             </form>
         </div>
     </div>
