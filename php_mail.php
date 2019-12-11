012565d0355e5ef9541c79b9175b6beb65fce67c
Siddharth Mishra <smishra1605@gmail.com>
2019-12-11 12:45:16 +0530

    Update php_mail.php

--- php_mail.php
+++ php_mail.php
@@ -1,12 +1,12 @@
-<?php  
-$to = "smishra1605@gmail.com";  
-$subject = "hellobro";  
-$body = "Happy Birthday";  
-$headers = "From: Peterrn";  
-$headers .= "Reply-To: abhajha@gmail.com";  
-$headers .= "Return-Path: abhajha@gmail.com";  
-$headers .= "X-Mailer: PHP5n";  
-$headers .= "MIME-Version: 1.0" . "n";  
-$headers .= "Content-type: text/html; charset=iso-8859-1" . "rn";  
-mail($to,$subject,$body,$headers);  
-?>  
+<?php
+$to = '@gmail.com';
+$subject = 'Hello from Siddharth!';
+$message = 'body';
+$headers = "From: smishra1605@gmail.com\r\n";
+if (mail($to, $subject, $message, $headers)) {
+   echo "SUCCESS. The mail is sent successfully";
+    echo "<script>setTimeout(\"location.href = 'html_main.php';\",6000);</script>"; 
+} else {
+   echo "ERROR";
+    echo "<script>setTimeout(\"location.href = 'html_main.php';\",6000);</script>"; 
+}
